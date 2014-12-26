6d70af8de4ea2bc386bb8715a3ed5bb377c4043a
Alexander <komrad.yasko@yandex.ru>
2014-12-26 12:54:03 +0200

    Initial commit

---
+++ EasyScreenshotCapture/Form1.Designer.cs
@@ -0,0 +1,243 @@
+﻿namespace EasyScreenshotCapture
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.btnHideToTray = new System.Windows.Forms.Button();
+            this.lvHistory = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.lvQueue = new System.Windows.Forms.ListView();
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lblInfo = new System.Windows.Forms.Label();
+            this.btnReassign = new System.Windows.Forms.Button();
+            this.lblStatusCounter = new System.Windows.Forms.Label();
+            this.lvMessages = new System.Windows.Forms.ListView();
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.tbPath = new System.Windows.Forms.TextBox();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.BalloonTipText = "Tray text";
+            this.notifyIcon1.BalloonTipTitle = "Info!";
+            this.notifyIcon1.Text = "Hello!";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // btnHideToTray
+            // 
+            this.btnHideToTray.Location = new System.Drawing.Point(18, 12);
+            this.btnHideToTray.Name = "btnHideToTray";
+            this.btnHideToTray.Size = new System.Drawing.Size(75, 23);
+            this.btnHideToTray.TabIndex = 0;
+            this.btnHideToTray.Text = "Hide to tray";
+            this.btnHideToTray.UseVisualStyleBackColor = true;
+            this.btnHideToTray.Click += new System.EventHandler(this.btnHideToTray_Click);
+            // 
+            // lvHistory
+            // 
+            this.lvHistory.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.lvHistory.GridLines = true;
+            this.lvHistory.Location = new System.Drawing.Point(6, 19);
+            this.lvHistory.Name = "lvHistory";
+            this.lvHistory.Size = new System.Drawing.Size(249, 228);
+            this.lvHistory.TabIndex = 2;
+            this.lvHistory.UseCompatibleStateImageBehavior = false;
+            this.lvHistory.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Action";
+            this.columnHeader1.Width = 104;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Key code";
+            this.columnHeader2.Width = 61;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Key value";
+            this.columnHeader3.Width = 66;
+            // 
+            // lvQueue
+            // 
+            this.lvQueue.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader4});
+            this.lvQueue.GridLines = true;
+            this.lvQueue.Location = new System.Drawing.Point(261, 19);
+            this.lvQueue.Name = "lvQueue";
+            this.lvQueue.Size = new System.Drawing.Size(127, 228);
+            this.lvQueue.TabIndex = 4;
+            this.lvQueue.UseCompatibleStateImageBehavior = false;
+            this.lvQueue.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Queue keys";
+            this.columnHeader4.Width = 117;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.groupBox1.Controls.Add(this.lblInfo);
+            this.groupBox1.Controls.Add(this.btnReassign);
+            this.groupBox1.Controls.Add(this.lblStatusCounter);
+            this.groupBox1.Controls.Add(this.lvHistory);
+            this.groupBox1.Controls.Add(this.lvMessages);
+            this.groupBox1.Controls.Add(this.lvQueue);
+            this.groupBox1.Location = new System.Drawing.Point(12, 43);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(558, 253);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Hotkeys tracking";
+            // 
+            // lblInfo
+            // 
+            this.lblInfo.AutoSize = true;
+            this.lblInfo.ForeColor = System.Drawing.Color.Red;
+            this.lblInfo.Location = new System.Drawing.Point(395, 206);
+            this.lblInfo.Name = "lblInfo";
+            this.lblInfo.Size = new System.Drawing.Size(148, 13);
+            this.lblInfo.TabIndex = 7;
+            this.lblInfo.Text = "Press new keys configuration!";
+            this.lblInfo.Visible = false;
+            // 
+            // btnReassign
+            // 
+            this.btnReassign.Location = new System.Drawing.Point(397, 224);
+            this.btnReassign.Name = "btnReassign";
+            this.btnReassign.Size = new System.Drawing.Size(155, 23);
+            this.btnReassign.TabIndex = 6;
+            this.btnReassign.Text = "Reassign hotkeys";
+            this.btnReassign.UseVisualStyleBackColor = true;
+            this.btnReassign.Click += new System.EventHandler(this.btnReassign_Click);
+            // 
+            // lblStatusCounter
+            // 
+            this.lblStatusCounter.AutoSize = true;
+            this.lblStatusCounter.Location = new System.Drawing.Point(394, 188);
+            this.lblStatusCounter.Name = "lblStatusCounter";
+            this.lblStatusCounter.Size = new System.Drawing.Size(88, 13);
+            this.lblStatusCounter.TabIndex = 5;
+            this.lblStatusCounter.Text = "Status counter: 0";
+            // 
+            // lvMessages
+            // 
+            this.lvMessages.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader5});
+            this.lvMessages.GridLines = true;
+            this.lvMessages.Location = new System.Drawing.Point(394, 19);
+            this.lvMessages.Name = "lvMessages";
+            this.lvMessages.Size = new System.Drawing.Size(158, 157);
+            this.lvMessages.TabIndex = 4;
+            this.lvMessages.UseCompatibleStateImageBehavior = false;
+            this.lvMessages.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "Message";
+            this.columnHeader5.Width = 150;
+            // 
+            // tbPath
+            // 
+            this.tbPath.Location = new System.Drawing.Point(100, 14);
+            this.tbPath.Name = "tbPath";
+            this.tbPath.Size = new System.Drawing.Size(383, 20);
+            this.tbPath.TabIndex = 6;
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(489, 12);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowse.TabIndex = 7;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // folderBrowserDialog1
+            // 
+            this.folderBrowserDialog1.Description = "Select path where you want to store yout screenshots";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(582, 308);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.tbPath);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnHideToTray);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Easy Screenshot Capture";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.Button btnHideToTray;
+        private System.Windows.Forms.ListView lvHistory;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ListView lvQueue;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label lblStatusCounter;
+        private System.Windows.Forms.ListView lvMessages;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.TextBox tbPath;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button btnReassign;
+        private System.Windows.Forms.Label lblInfo;
+    }
+}
+
---
+++ EasyScreenshotCapture/Form1.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EasyScreenshotCapture
+{
+    public partial class Form1 : Form
+    {
+        Utilities.GlobalKeyboardHook hook = new Utilities.GlobalKeyboardHook();
+
+        string save_path = "";
+        bool hotkeys_reassigning = false;
+
+        Queue<int> q_hotkeys = new Queue<int>();
+        List<int> hotkeys_save_screenshot = new List<int>();
+        System.Media.SoundPlayer sound_player = new System.Media.SoundPlayer();
+
+        List<int> l_hotkeys_register = new List<int>();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Hotkeys to hook
+            hotkeys_save_screenshot.Add((int)Keys.LControlKey);
+            hotkeys_save_screenshot.Add((int)Keys.PrintScreen);
+            // ---------------
+
+            hook.KeyDown += hook_KeyDown;
+            hook.KeyUp += hook_KeyUp;
+
+            notifyIcon1.Icon = SystemIcons.WinLogo;
+        }
+
+        void hook_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (l_hotkeys_register.Contains(e.KeyValue))
+            {
+                l_hotkeys_register.Remove(e.KeyValue);
+            }
+            lvHistory.Items.Add("KEY_UP");
+            lvHistory.Items[lvHistory.Items.Count - 1].SubItems.Add(e.KeyData.ToString());
+            lvHistory.Items[lvHistory.Items.Count - 1].SubItems.Add(e.KeyValue.ToString());
+
+            lvHistory.EnsureVisible(lvHistory.Items.Count - 1);
+
+            lblStatusCounter.Text = String.Format("Status counter: " + l_hotkeys_register.Count.ToString());
+
+            if (l_hotkeys_register.Count == 0)
+            {
+                ProcessHotkeys();
+            }
+        }
+
+        private void ProcessHotkeys()
+        {
+            bool handled = true;
+            if (hotkeys_reassigning == false)
+            {
+                if (q_hotkeys.Count == hotkeys_save_screenshot.Count)
+                {
+                    for (int i = 0; q_hotkeys.Count != 0; i++ )
+                    {
+                        if (q_hotkeys.Dequeue() != hotkeys_save_screenshot[i])
+                        {
+                            handled = false;
+                            break;
+                        }
+                    }
+
+                    if (handled)
+                    {
+                        sound_player.Play();
+                        SaveScreenshot(save_path);
+
+                        lvMessages.Items.Add("Screen captured!");
+                        lvMessages.EnsureVisible(lvMessages.Items.Count - 1);
+                        notifyIcon1.ShowBalloonTip(1000, "Information", "Screen captured!", ToolTipIcon.Info);
+                    }
+                }
+            }
+            else
+            {
+                hotkeys_save_screenshot.Clear();
+                while (q_hotkeys.Count != 0)
+                {
+                    hotkeys_save_screenshot.Add(q_hotkeys.Dequeue());
+                }
+                hotkeys_reassigning = false;
+                lblInfo.Visible = false;
+                btnReassign.Enabled = true;
+            }
+            q_hotkeys.Clear();
+        }
+
+        void hook_KeyDown(object sender, KeyEventArgs e)
+        {
+            lvHistory.Items.Add("Key DOWN: " + e.KeyValue.ToString());
+            lvHistory.Items[lvHistory.Items.Count - 1].SubItems.Add(e.KeyData.ToString());
+            lvHistory.Items[lvHistory.Items.Count - 1].SubItems.Add(e.KeyValue.ToString());
+            lvHistory.EnsureVisible(lvHistory.Items.Count - 1);
+
+            if (!q_hotkeys.Contains((int)e.KeyCode))
+            {
+                if(!l_hotkeys_register.Contains(e.KeyValue))
+                {
+                    l_hotkeys_register.Add(e.KeyValue);
+                }
+                q_hotkeys.Enqueue((int)e.KeyCode);
+                lvQueue.Items.Clear();
+                foreach(int i in q_hotkeys)
+                {
+                    lvQueue.Items.Add(i.ToString());
+                }
+            }
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            if(e.Button == System.Windows.Forms.MouseButtons.Left)
+            {
+                if (this.WindowState == FormWindowState.Minimized)
+                {
+                    Show();
+                    this.WindowState = FormWindowState.Normal;
+                }
+            }
+        }
+
+        public void SaveScreenshot(string path)
+        {
+            Bitmap bmp = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
+            Graphics gr = Graphics.FromImage(bmp);
+            gr.CopyFromScreen(0, 0, 0, 0, bmp.Size);
+            bmp.Save(path + String.Format("//SCREEN{0}{1}{2}{3}{4}{5}.png", DateTime.Now.Day, DateTime.Now.Month, DateTime.Now.Year, DateTime.Now.Hour, DateTime.Now.Minute, DateTime.Now.Second));
+        }
+
+        private void btnHideToTray_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+            Hide();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            save_path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            tbPath.Text = save_path;
+
+            sound_player.SoundLocation = "Data//screenshot.wav";
+            sound_player.Load();
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            if(folderBrowserDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                save_path = folderBrowserDialog1.SelectedPath;
+                tbPath.Text = save_path;
+            }
+        }
+
+        private void btnReassign_Click(object sender, EventArgs e)
+        {
+            hotkeys_reassigning = true;
+            lblInfo.Visible = true;
+            btnReassign.Enabled = false;
+        }
+    }
+}
